ea8675ded238dce37ab87b2c9b9044560ccd2e15
byTxampi <inigogasti@gmail.com>
2021-02-13 15:44:04 +0100

    points added

--- bird.py
+++ bird.py
@@ -9,6 +9,8 @@ class Bird:
        self.y = 100
        self.color = (255,255,255)
        self.speed = -0.5
+       self.points = 0
+       self.hit = False
 
     def draw(self, screen):
         if(self.y <= 0):
@@ -16,8 +18,10 @@ class Bird:
         pygame.draw.circle(screen, self.color, (self.x, self.y), self.size)
     
     def move(self):
-        self.speed = self.speed - 0.2
-        self.y = self.y - self.speed
+        if(not self.hit):
+            self.speed = self.speed - 0.2
+            self.y = self.y - self.speed
+            self.points = self.points + 0.1
     
     def up(self):
         self.speed = 5.0
--- main.py
+++ main.py
@@ -9,10 +9,13 @@ from pygame.locals import *
 def hitBox(bird):
     if(bird.y+15 >= 700):
         bird.color=(0,0,0)
+        bird.hit = True
     if(bird.y+15 >= rect2.y and bird.x+15 >= rect2.x and bird.x <= rect2.x+rwidht):
         bird.color=(0,0,0)
+        bird.hit = True
     if(bird.y-15 <= rect1.height and bird.x+15 >= rect1.x and bird.x <= rect1.x+rwidht):
         bird.color=(0,0,0)
+        bird.hit = True
 
 
 
@@ -28,7 +31,7 @@ xpos = 300
 (rwidht, rheight) = (50, 250)
 
 rect1 = pygame.Rect(xpos,0,rwidht,rheight)
-rect2 = pygame.Rect(xpos,rheight+100,rwidht,700)
+rect2 = pygame.Rect(xpos,rheight+115,rwidht,700)
 
 clock = pygame.time.Clock()
 
@@ -58,7 +61,7 @@ while True:
     if(rect1.x < -rwidht):
         rect1.x = 550
         rect2.x = 550
-        rect1.height = random.randrange(50,650)
+        rect1.height = random.randrange(100,500)
         rect2.y = rect1.height+75
     hitBox(bird)
     clock.tick(60)
