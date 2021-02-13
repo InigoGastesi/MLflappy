e3bedc9e601aed079a99b3d8145967ab92392aac
byTxampi <inigogasti@gmail.com>
2021-02-13 15:13:12 +0100

    hitbox and up move

--- bird.py
+++ bird.py
@@ -4,10 +4,20 @@ import pygame
 class Bird:
     def __init__(self, id, height):
        self.id = id 
-       self.size = height*0.03
+       self.size = 15
        self.x = 100
        self.y = 100
        self.color = (255,255,255)
+       self.speed = -0.5
 
     def draw(self, screen):
+        if(self.y <= 0):
+            self.y = 0
         pygame.draw.circle(screen, self.color, (self.x, self.y), self.size)
+    
+    def move(self):
+        self.speed = self.speed - 0.2
+        self.y = self.y - self.speed
+    
+    def up(self):
+        self.speed = 5.0
--- main.py
+++ main.py
@@ -1,15 +1,34 @@
 import pygame
+import keyboard
+import random
 from bird import Bird
 import time
 import sys
 from pygame.locals import *
+
+def hitBox(bird):
+    if(bird.y+15 >= 700):
+        bird.color=(0,0,0)
+    if(bird.y+15 >= rect2.y and bird.x+15 >= rect2.x and bird.x <= rect2.x+rwidht):
+        bird.color=(0,0,0)
+    if(bird.y-15 <= rect1.height and bird.x+15 >= rect1.x and bird.x <= rect1.x+rwidht):
+        bird.color=(0,0,0)
+
+
+
+
+
+
+
+
+
 (width, height) = (500,700)
 
 xpos = 300
 (rwidht, rheight) = (50, 250)
 
 rect1 = pygame.Rect(xpos,0,rwidht,rheight)
-rect2 = pygame.Rect(xpos,rheight+75,rwidht,700)
+rect2 = pygame.Rect(xpos,rheight+100,rwidht,700)
 
 clock = pygame.time.Clock()
 
@@ -18,10 +37,6 @@ bird = Bird(1, height)
 
 background = (0, 0, 0)
 screen = pygame.display.set_mode((width,height))
-screen.fill(background)
-
-
-
 
 
 while True:
@@ -36,5 +51,18 @@ while True:
     pygame.draw.rect(screen, (255,255,255), rect2)
     rect1.move_ip(-speed,0)
     rect2.move_ip(-speed,0)
+    bird.move()
+    if(keyboard.is_pressed('space')):
+        bird.up()
+
+    if(rect1.x < -rwidht):
+        rect1.x = 550
+        rect2.x = 550
+        rect1.height = random.randrange(50,650)
+        rect2.y = rect1.height+75
+    hitBox(bird)
     clock.tick(60)
     pygame.display.update()
+
+
+
